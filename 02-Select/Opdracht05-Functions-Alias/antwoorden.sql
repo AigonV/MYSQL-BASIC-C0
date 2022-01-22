f6aef3fe936fe8264a529decbc86996aab82528b
Mohammed Ahmed <2149869@talnet.nl>
2022-01-22 01:11:21 +0100

    Functies (niet af)

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,9 +1,9 @@
 -- Opdracht 1 
-
+SELECT MAX(wage) AS hoogste_loon_speler_fc_utrecht FROM players WHERE club="FC Utrecht"
 -- Opdracht 2 
-
+SELECT AVG(wage) AS gemiddeld_inkomen_van_alle_spelers FROM players
 -- Opdracht 3
-
+SELECT SUM(wage) AS som_van_fc_groningen FROM players WHERE club="FC Groningen"
 -- Opdracht 4
 
 -- Opdracht 5
